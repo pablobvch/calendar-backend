b10b849b466a7566c3c11d0499ae56836bfb63ad
Pablo Alaniz <pablobvch@gmail.com>
2022-09-19 14:52:27 -0300

    fix: add generic request to index

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ app.use(express.json());
 
 app.use(`/api/auth`, require("./routes/auth"));
 app.use(`/api/events`, require("./routes/events"));
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
